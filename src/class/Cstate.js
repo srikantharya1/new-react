1f7011e49f8e3a5853df4a03116b3bc0a910f8ad
Srikanth Arya <aryasrikanth73@gmai.com>
2024-02-07 20:10:56 +0530

    hi

--- src/class/Cstate.js
+++ src/class/Cstate.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react'
+import Cprops from './Cprops'
 
 export default class Cstate extends Component {
+    state = {
+        sentence:"This is a STATE of Class Component",
+        sentence1:"This is a PROPS of Class Component"
+    }
   render() {
     return (
-      <div>Cstate</div>
+      <div>
+        <center>
+            <h1>{this.state.sentence}</h1>
+            <Cprops name={this.state.sentence1}/>
+        </center>
+      </div>
     )
   }
 }
