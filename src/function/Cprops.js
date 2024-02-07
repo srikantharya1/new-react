ec282d472816e4a037a78b41381c15d7e2fe3c75
Srikanth Arya <aryasrikanth73@gmai.com>
2024-02-07 22:39:56 +0530

    useState

---
+++ src/function/Cprops.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cprops = () => {
+  return (
+    <div>Cprops</div>
+  )
+}
---
+++ src/function/Cstate.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Cstate = () => {
+  return (
+    <div>Cstate</div>
+  )
+}
